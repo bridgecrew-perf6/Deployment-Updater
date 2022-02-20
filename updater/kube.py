b733250c4f76245a291f0a413956f404e6fa01ff
Frazer <frazzles740@gmail.com>
2022-02-20 15:30:55 +0000

    fix find index

--- updater/kube.py
+++ updater/kube.py
@@ -9,7 +9,7 @@ apps = client.AppsV1Api()
 
 
 def _find_index(items, filter):
-    if not isinstance(filter, callable):
+    if not isinstance(filter, function):
         def _filter(item):
             return item == filter
     else:
